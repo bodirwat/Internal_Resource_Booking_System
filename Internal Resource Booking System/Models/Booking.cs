482fe856a04345da23d89c447c594230e0100459
bodirwat <bodirwatlabo@outlook.com>
2025-07-16 22:59:45 +0200

    Update project solution and core ASP.NET MVC project files

---
+++ Internal Resource Booking System/Models/Booking.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Internal_Resource_Booking_System.Models;
+
+public partial class Booking
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Resource is now now required")]
+    public int ResourceId { get; set; }
+    [Range(typeof(DateTime), "1/1/1753", "12/31/9999", ErrorMessage = "End Time must be between 1/1/1753 and 12/31/9999")]
+    [Required(ErrorMessage = "Start Time is required")]
+    public DateTime StartTime { get; set; }
+
+    [Range(typeof(DateTime), "1/1/1753", "12/31/9999", ErrorMessage = "End Time must be between 1/1/1753 and 12/31/9999")]
+    [Required(ErrorMessage = "End Time is required")]
+    public DateTime EndTime { get; set; }
+
+    [StringLength(100, ErrorMessage = "Booked By name cannot exceed 100 characters")]
+    public string? BookedBy { get; set; }
+
+    [StringLength(255, ErrorMessage = "Purpose cannot exceed 255 characters")]
+    public string? Purpose { get; set; }
+
+    public virtual Resource? Resource { get; set; }
+}
+
+
+public class ResourceDetailsViewModel
+{
+    public Resource Resource { get; set; }
+    public List<Booking> Bookings { get; set; }
+}
+
+
---
+++ Internal Resource Booking System/Models/Resource.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Internal_Resource_Booking_System.Models;
+
+public partial class Resource
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Name is required.")]
+    [StringLength(100, ErrorMessage = "Name cannot exceed 100 characters.")]
+    public string Name { get; set; } = null!;
+
+    [Required(ErrorMessage = "Description is required.")]
+    [StringLength(500, ErrorMessage = "Description cannot exceed 500 characters.")]
+    public string Description { get; set; } = null!;
+
+    [Required(ErrorMessage = "Location is required.")]
+    [StringLength(100, ErrorMessage = "Location cannot exceed 100 characters.")]
+    public string Location { get; set; } = null!;
+
+    [Required(ErrorMessage = "Capacity is required.")]
+    [Range(1, 1000, ErrorMessage = "Capacity must be at least 1.")]
+    public int Capacity { get; set; }
+
+    [Required(ErrorMessage = "Availability is required.")]
+    public bool IsAvailable { get; set; } = true;
+
+    public virtual ICollection<Booking> Bookings { get; set; } = new List<Booking>();
+}
+public class HomeViewModel
+{
+    public int TotalBookings { get; set; }
+    public int TotalResources { get; set; }
+    public int BookingsToday { get; set; }
+    public List<Booking> RecentBookings { get; set; }
+}
